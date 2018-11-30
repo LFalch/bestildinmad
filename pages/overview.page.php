f757f304a2e8770d57924b66ff6273e001520c41
LFalch <lucas@wasd.dk>
2018-11-30 09:05:16 +0100

    Fix overview

--- pages/overview.page.php
+++ pages/overview.page.php
@@ -1,73 +1,40 @@
 <?php
+include 'inc/mysql.php';
 
 function section(){ ?>
 <h1 class="title">Oversigt</h1>
 <table class="table">
     <thead>
       <tr>
-        <th>pris</th>
         <th>Vare</th>
+        <th>Billede</th>
+        <th>Pris</th>
         <th>Antal</th>
       </tr>
     </thead>
     <tbody>
       <?php
+        $conn = new_conn();
+        $query = $conn->query('SELECT `id`, `name`, `price`, `img_file`, `stock` FROM vare');
+        $vareArray = $query->fetch_all();
+
         for ($i=0; $i < count($vareArray); $i++) {
           $id = $vareArray[$i][0];
+          $name = $vareArray[$i][1];
+          $pris = $vareArray[$i][2];
+          $img = $vareArray[$i][3];
+          $stock = $vareArray[$i][4];
 
-          $query = $conn->query("SELECT `id`, `name`, `price`, `img_file` FROM vare");
-          $bestil = $query->fetch_all();
-
-          for ($j=0; $j < count($vare); $j++) {
-            echo "<tr>";
-            echo "<td>$id</td>";
-            echo "<td>".$vare[$j][1]."</td>";
-            echo "<td>".$vare[$j][2]."</td>";
-            echo "</tr>";
-          }
+          echo "<tr>";
+          echo "<td>$name</td>";
+          echo "<td><img src=\"/img/$img\" class=\"image is-64x64\"></td>";
+          echo "<td>$pris</td>";
+          echo "<td>$stock</td>";
+          echo "</tr>";
         }
       ?>
     </tbody>
   </table>
-<?php } else { ?>
-  <p>Log venligst ind for at bestille mad</p>
-<?php }
-}
-
-<table class="table">
- 
-
-
-    
-    <tr>
-      <th>Panini</th>
-      <td><img src="/img/panini.jpg" class="image is-64x64"></td>
-      <td>25 kr</td>
-      <td> </td>
-      <td> </td>
-      <td> </td>
-      <td> </td>
-      <td> </td>
-      <td> </td>
-      <td> </td>
-      <td><a href="">Tilføj</a></td>
-    </tr>
-      
-    <tr>
-      <th>Kylling/Bacon sandwich</th>
-      <td><img src="/img/kbsandwich.jpg" class="image is-64x64"></td>
-      <td>25 kr</td>
-      <td> </td>
-      <td> </td>
-      <td> </td>
-      <td> </td>
-      <td> </td>
-      <td> </td>
-      <td> </td>
-      <td><a href="">Tilføj</a></td>
-    </tr>
-</table>
-
 <?php }
 
 function title(){
