5361dc6f1e1273ea4ca55ae9313769de77982e66
LFalch <lucas@wasd.dk>
2018-11-30 20:18:48 +0100

    Fix error page

--- pages/error.page.php
+++ pages/error.page.php
@@ -1,26 +1,26 @@
 <?php
-$errorType = $_GET['e'];
-$title = "";
-$body = "";
-
-switch ($errorType) {
-  case 'user':
-  $title = 'Forkert brugernavn';
-  $body = '<p>Det indtastede brugernavn kunne ikke findes.</p>';
-  break;
-  case 'passwd':
-  $title = 'Forkert kode';
-  $body = '<p>Den indtastede adgangskode matchede ikke.</p>';
-  break;
-  default:
-  $body = "<p>Der skete en ukendt fejl.</p>";
-}
 
 function title(){
   echo "Bestil Din Mad - Fejl";
 }
 
-function section(){ ?>
+function section(){
+    $errorType = $_GET['e'] or '';
+    $title = "";
+    $body = "";
+
+    switch ($errorType) {
+        case 'user':
+        $title = 'Forkert brugernavn';
+        $body = '<p>Det indtastede brugernavn kunne ikke findes.</p>';
+        break;
+        case 'passwd':
+        $title = 'Forkert kode';
+        $body = '<p>Den indtastede adgangskode matchede ikke.</p>';
+        break;
+        default:
+        $body = "<p>Der skete en ukendt fejl.</p>";
+    } ?>
         <h1 class="title">
           Fejl
           <?php
